3fde550f8c085cd407154809d588825026ff5a34
Johand <johand@aptika.co.id>
2023-07-04 12:10:39 +0700

    fix like in instagram

--- socmed/instagram/like.js
+++ socmed/instagram/like.js
@@ -4,49 +4,61 @@ const url_adspower = process.env.URL_ADSPOWER;
 const axios = require("axios");
 
 async function like_ig(req, res) {
-  const { user_id, post_link } = req.body;
-
-  if (!user_id) {
-    throw { message: "User Id is required" };
-  }
-
-  if (!post_link) {
-    throw { message: "Post link is required" };
-  }
-
-  const { data } = await axios.get(`${url_adspower}${user_id}`);
-
-  const puppeteerUrl = data.data.ws.puppeteer;
-
-  const browser = await puppeteer.connect({
-    browserWSEndpoint: puppeteerUrl,
-    defaultViewport: null,
-  });
-
-  const pages = await browser.pages(0);
-
-  const page = pages[0];
-
-  await page.goto(`https://www.instagram.com/p/${post_link}`);
-
-  let final_result
-
   try {
-    await page.evaluate(() => {
-      const likeButton = document.querySelector(
-        'div.x1i10hfl.x6umtig.x1b1mbwd.xaqea5y.xav7gou.x9f619.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x16tdsg8.x1hl2dhg.xggy1nq.x1a2a7pz.x6s0dn4.xjbqb8w.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x1ypdohk.x78zum5.xl56j7k.x1y1aw1k.x1sxyh0.xwib8y2.xurb0ha span svg[aria-label="Suka"]'
-      );
-      likeButton.parentElement.parentElement.parentElement.click();
-      final_result = "Sucess Like"
+    const { user_id, post_link } = req.body;
+
+    if (!user_id) {
+      throw { message: "User Id is required" };
+    }
+
+    if (!post_link) {
+      throw { message: "Post link is required" };
+    }
+
+    const { data } = await axios.get(`${url_adspower}${user_id}`);
+
+    const puppeteerUrl = data.data.ws.puppeteer;
+
+    const browser = await puppeteer.connect({
+      browserWSEndpoint: puppeteerUrl,
+      defaultViewport: null,
     });
 
+    const pages = await browser.pages(0);
+
+    const page = pages[0];
+
+    await page.goto(`https://www.instagram.com/p/${post_link}`);
+
+    const titleValues = await page.$$eval("title", (elements) => {
+      return elements.map((element) => element.textContent.trim());
+    });
+
+    const containsBatalSuka = titleValues.includes("Batal Suka");
+
+    console.log(containsBatalSuka);
+
+    let final_result;
+
+    if (containsBatalSuka) {
+      final_result = "You already like this instagram post";
+    } else {
+      await page.reload()
+      await page.evaluate(() => {
+        const likeButton = document.querySelector(
+          'div.x1i10hfl.x6umtig.x1b1mbwd.xaqea5y.xav7gou.x9f619.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x16tdsg8.x1hl2dhg.xggy1nq.x1a2a7pz.x6s0dn4.xjbqb8w.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x1ypdohk.x78zum5.xl56j7k.x1y1aw1k.x1sxyh0.xwib8y2.xurb0ha span svg[aria-label="Suka"]'
+        );
+        likeButton.parentElement.parentElement.parentElement.click();
+      });
+      final_result = "You like this post";
+    }
+
+    res.status(200).json(global_response("SUCCESS", 200, final_result));
+
+    // console.log(ariaLabel);
   } catch (error) {
-    console.log(error)
-    final_result = "You already like this post"
+    console.log(error);
   }
-
-  res.status(200).json(global_response("SUCCESS", 200, final_result));
-
 }
 
 module.exports = like_ig;
