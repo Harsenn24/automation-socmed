c1a1548b4d9f9b82aafab2e39f895a27b2a9e6d1
Johand <johand@aptika.co.id>
2023-07-04 10:36:22 +0700

    finish instagram like

--- router/instagram.js
+++ router/instagram.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const comment_ig = require("../socmed/instagram/comment");
-const multiple_comment_ig = require("../socmed/instagram/multiple_command");
-const multiple_comment_ig_test = require("../socmed/instagram/multi.command");
+const like_ig = require("../socmed/instagram/like");
 const ig_router = express.Router();
 
 ig_router.get("/admin-socmed/instagram/comment", comment_ig);
-ig_router.get("/admin-socmed/instagram/comment-multiple", multiple_comment_ig);
+ig_router.get("/admin-socmed/instagram/like", like_ig);
 
 module.exports = ig_router;
