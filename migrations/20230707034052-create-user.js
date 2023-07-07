5348fd8e60d7706c164a85e40d0cfe069cd23ce4
Johand <johand@aptika.co.id>
2023-07-07 10:54:46 +0700

    set up database finish

---
+++ migrations/20230707034052-create-user.js
@@ -0,0 +1,36 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up(queryInterface, Sequelize) {
+    return queryInterface.createTable("Users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      user_id: {
+        type: Sequelize.STRING,
+      },
+      running: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true,
+      },
+      detail: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down(queryInterface, Sequelize) {
+    return queryInterface.dropTable("Users");
+  },
+};
