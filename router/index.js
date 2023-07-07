c7b93a2081fb2c74c87d0b330a6d0abd320dc97d
Johand <johand@aptika.co.id>
2023-07-07 11:13:55 +0700

    success find all user but without pagination

--- router/index.js
+++ router/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const ig_router = require("./instagram");
 const fb_router = require("./facebook");
+const user_router = require("./user_data");
 const router = express.Router();
 
+router.use(user_router);
 router.use(ig_router);
 router.use(fb_router);
 
