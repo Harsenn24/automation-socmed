ad7c20cffedf94e1c804c1e6847c784203399a49
Johand <johand@aptika.co.id>
2023-07-07 11:27:13 +0700

    success read detail of user

---
+++ controller/read_detail_user.js
@@ -0,0 +1,39 @@
+const global_response = require("../global_response");
+const { User } = require("../models/index");
+const { Sequelize } = require('sequelize');
+
+
+async function read_detail_user(req, res) {
+  try {
+    const { id } = req.params;
+
+    const result_find = await User.findByPk(id, {
+      attributes: [
+        "id",
+        "user_id",
+        "running",
+        [
+          Sequelize.literal(
+            'CASE WHEN detail IS NULL THEN "-" ELSE detail END'
+          ),
+          "detail",
+        ],
+        [
+          Sequelize.literal('DATE_FORMAT(createdAt, "%Y-%m-%d %H:%i:%s")'),
+          "createdAt",
+        ],
+        [
+          Sequelize.literal('DATE_FORMAT(createdAt, "%Y-%m-%d %H:%i:%s")'),
+          "updatedAt",
+        ],
+      ],
+    });
+
+    res.status(200).json(global_response("Success", 200, result_find));
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(global_response("Failed", 400, error));
+  }
+}
+
+module.exports = read_detail_user;
--- router/user_data.js
+++ router/user_data.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const read_all_user = require("../controller/read.user");
+const read_detail_user = require("../controller/read_detail_user");
 const user_router = express.Router();
 
 user_router.get("/all-user", read_all_user);
+
+user_router.get("/all-user/:id", read_detail_user);
+
 
 module.exports = user_router;
