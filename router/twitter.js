aa4c6315894c51705f9124e1766af67f5515c430
Johand <johand@aptika.co.id>
2023-07-25 15:52:06 +0700

    retweet twitter done

--- router/twitter.js
+++ router/twitter.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const follow_twitter = require("../socmed/twitter/follow");
 const status_twitter = require("../controller/twitter/status");
+const retweet_comment_twitter = require("../controller/twitter/retweet.comment");
 const twitter_router = express.Router();
 
 twitter_router.get("/admin-socmed/twitter/follow", follow_twitter);
 
 twitter_router.get("/admin-socmed/twitter/status", status_twitter);
+
+twitter_router.get(
+  "/admin-socmed/twitter/retweet-comment",
+  retweet_comment_twitter
+);
 
 module.exports = twitter_router;
