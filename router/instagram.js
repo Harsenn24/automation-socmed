a60f8615e4a1b73d4549bbd1394b74fb52af9929
Johand <johand@aptika.co.id>
2023-07-28 16:00:02 +0700

    intagram comment done

--- router/instagram.js
+++ router/instagram.js
@@ -1,13 +1,10 @@
 const express = require("express");
-const comment_ig = require("../socmed/instagram/comment");
-const comment_ig_multiple = require("../socmed/instagram/multiple.comment");
 const follow_instagram = require("../controller/instagram/follow");
 const like_instagram = require("../controller/instagram/like");
+const comment_instagram = require("../controller/instagram/comment");
 const ig_router = express.Router();
 
-ig_router.get("/admin-socmed/instagram/comment", comment_ig);
-
-ig_router.get("/admin-socmed/instagram/comment-multiple", comment_ig_multiple);
+ig_router.get("/admin-socmed/instagram/comment", comment_instagram);
 
 ig_router.get("/admin-socmed/instagram/like", like_instagram);
 
